7f338ef4c82a1018d4cd8f26905cfc75b3b34818
manchalaabhilash <abhisunny787@gmail.com>
2025-08-15 13:46:06 +0000

    colab commit

---
+++ mlops/hosting/hosting.py
@@ -0,0 +1,12 @@
+from huggingface_hub import HfApi
+import os
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+
+api.upload_folder(
+    folder_path="mlops/deployment",     # the local folder containing your files
+    repo_id="abhilashmanchala/wellness_tourism_prediction",          # the target repo
+    repo_type="space",                      # dataset, model, or space
+    path_in_repo="",                          # optional: subfolder path inside the repo
+    token = api
+)
